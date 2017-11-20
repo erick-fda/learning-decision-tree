982f02a2464918d990a60e93c316a49ded997775
Erick FdA <erick.fernandez.da@gmail.com>
2017-11-20 15:26:51 -0800

    Started reading training file.

--- COMP8901_Asg05/COMP8901_Asg05/FileReader.cs
+++ COMP8901_Asg05/COMP8901_Asg05/FileReader.cs
@@ -16,8 +16,11 @@
 /*========================================================================================
 	Dependencies
 ========================================================================================*/
+using SysRegex = System.Text.RegularExpressions.Regex;
+using SysGeneric = System.Collections.Generic;
+using SysConsole = System.Console;
 
-namespace MyProgram
+namespace COMP8901_Asg05
 {
     /*====================================================================================
         FileReader
@@ -25,12 +28,14 @@ namespace MyProgram
     /**
         Reads in the content of a file for the AI decision tree builder.
     */
-    public class FileReader
+    public static class FileReader
     {
         /*--------------------------------------------------------------------------------
             Instance Fields
         --------------------------------------------------------------------------------*/
-
+        private const char LINE_BREAK = '\n';
+        private const char ATTRIBUTE_SEPARATOR = ' ';
+        public const string HORIZONTAL_RULE = "-----------------------------------";
 
         /*--------------------------------------------------------------------------------
             Instance Properties
@@ -43,8 +48,100 @@ namespace MyProgram
 
 
         /*--------------------------------------------------------------------------------
-            Instance Methods
+            Class Methods
         --------------------------------------------------------------------------------*/
+        public static void ReadDataFile(string filePath)
+        {
+            string fileContents = System.IO.File.ReadAllText(filePath);
 
+            /* Remove carriage returns and replace tabs with spaces. */
+            fileContents = SysRegex.Replace(fileContents, "\r", "");
+            fileContents = SysRegex.Replace(fileContents, "\t", " ");
+
+            /* Split into lines. */
+            string[] lines = fileContents.Split(LINE_BREAK);
+
+            /* Get rid of blank and comment lines. */
+            SysGeneric.List<string> dataLines = new SysGeneric.List<string>();
+            foreach (string eachLine in lines)
+            {
+                if (eachLine.Length < 1)
+                {
+                    continue;
+                }
+
+                if (eachLine[0].Equals('/') &&
+                    eachLine[1].Equals('/'))
+                {
+                    continue;
+                }
+
+                /* If the line is neither blank nor commented, then it is data. */
+                dataLines.Add(eachLine);
+            }
+
+            /* Extract data from the data lines. */
+            bool areClassificationsRead = false;
+            bool isAttributeCountRead = false;
+            bool areAttributesRead = false;
+            bool isDataCountRead = false;
+            bool isDataRead = false;
+            int attributeCount = 0;
+            int dataCount = 0;
+
+            SysConsole.Write(System.String.Format("{0}\n\tReading Classifications\n{0}\n", HORIZONTAL_RULE));
+            foreach (string eachLine in dataLines)
+            {
+                /* Read the classifications. */
+                if (!areClassificationsRead)
+                {
+                    /* Are we done reading the classifications? */
+                    if (int.TryParse(eachLine, out attributeCount))
+                    {
+                        areClassificationsRead = true;
+                        isAttributeCountRead = true;
+                        SysConsole.Write(System.String.Format("{0} classifications read.\n\n", COMP8901_Asg05._classifications.Count));
+                        SysConsole.Write(System.String.Format("{0}\n\tReading Attributes\n{0}\n", HORIZONTAL_RULE));
+                        SysConsole.Write(System.String.Format("Attribute count: {0}\n", attributeCount));
+                        continue;
+                    }
+
+                    COMP8901_Asg05._classifications.Add(eachLine);
+                    SysConsole.Write(eachLine + "\n");
+                    continue;
+                }
+
+                /* Read the attributes. */
+                if (!areAttributesRead)
+                {
+                    /* Are we done reading the attributes? */
+                    if (int.TryParse(eachLine, out dataCount))
+                    {
+                        areAttributesRead = true;
+                        isDataCountRead = true;
+                        SysConsole.Write(System.String.Format("{0} attributes read.\n\n", COMP8901_Asg05._attributes.Count));
+                        SysConsole.Write(System.String.Format("{0}\n\tReading Data\n{0}\n", HORIZONTAL_RULE));
+                        SysConsole.Write(System.String.Format("Data count: {0}\n\n", dataCount));
+                        continue;
+                    }
+
+                    /* Parse each attribute. */
+                    string[] attributeParts = eachLine.Split(ATTRIBUTE_SEPARATOR);
+                    SysGeneric.List<string> attributeValues = new SysGeneric.List<string>();
+
+                    for (int i = 1; i < attributeParts.Length; i++)
+                    {
+                        attributeValues.Add(attributeParts[i]);
+                    }
+
+                    COMP8901_Asg05._attributes[attributeParts[0]] = attributeValues;
+                    SysConsole.Write(eachLine + "\n");
+                }
+
+
+            }
+
+            /*  */
+        }
     }
 }
--- COMP8901_Asg05/COMP8901_Asg05/Program.cs
+++ COMP8901_Asg05/COMP8901_Asg05/Program.cs
@@ -16,7 +16,8 @@
 /*========================================================================================
 	Dependencies
 ========================================================================================*/
-
+using SysGeneric = System.Collections.Generic;
+using SysConsole = System.Console;
 
 /*========================================================================================
 	COMP8901_Asg05
@@ -36,20 +37,52 @@ namespace COMP8901_Asg05
         /*--------------------------------------------------------------------------------
             Class Properties
         --------------------------------------------------------------------------------*/
-
+        private static string _trainingFilePath { get; set; }
+        private static string _testFilePath { get; set; }
+        public static SysGeneric.List<string> _classifications { get; set; }
+        public static SysGeneric.Dictionary<string, SysGeneric.List<string>> _attributes { get; set; }
 
         /*--------------------------------------------------------------------------------
             Main Method
         --------------------------------------------------------------------------------*/
         static void Main(string[] args)
         {
+            Init();
+            ReadArgs(args);
 
+            SysConsole.Write("Press any key to exit...");
+            SysConsole.ReadKey();
         }
 
         /*--------------------------------------------------------------------------------
             Class Methods
         --------------------------------------------------------------------------------*/
+        /**
+            Initializes class fields and properties.
+        */
+        private static void Init()
+        {
+            _classifications = new SysGeneric.List<string>();
+            _attributes = new SysGeneric.Dictionary<string, SysGeneric.List<string>>();
+        }
 
+        /**
+            Parses the arguments to the program.
+        */
+        private static void ReadArgs(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                SysConsole.Write("ERROR: Not enough arguments.\n\n");
+            }
+
+            _trainingFilePath = args[0];
+            _testFilePath = args[1];
+
+            /* Get test data from training file. */
+            SysConsole.Write("Reading training file...\n\n");
+            FileReader.ReadDataFile(_trainingFilePath);
+        }
     }
 }
 
